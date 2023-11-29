5fe0d6d2c30f1e5aefaf954c707a70514e94d92d
James Greiner <jamesdgreiner@icloud.com>
2023-11-28 20:45:57 -0800

    Initial Commit

---
+++ DiabetesTracker/DiabetesTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  DiabetesTrackerApp.swift
+//  DiabetesTracker
+//
+//  Created by James Greiner on 11/28/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DiabetesTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
