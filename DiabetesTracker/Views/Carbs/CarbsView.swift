5ec48a2c5a034942d2a73938d920044deb48513e
James Greiner <jamesdgreiner@icloud.com>
2023-12-21 00:37:08 -0800

    Add initial set of tabs and view folders

---
+++ DiabetesTracker/Views/Carbs/CarbsView.swift
@@ -0,0 +1,18 @@
+//
+//  CarbsView.swift
+//  DiabetesTracker
+//
+//  Created by James Greiner on 12/20/23.
+//
+
+import SwiftUI
+
+struct CarbsView: View {
+    var body: some View {
+        Text("CARBS")
+    }
+}
+
+#Preview {
+    CarbsView()
+}
