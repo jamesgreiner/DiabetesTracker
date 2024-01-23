16430980117367fd29cdad11bcbe94947a24a345
James Greiner <jamesdgreiner@icloud.com>
2024-01-22 21:45:10 -0800

    Add file for glucose trend chart

---
+++ DiabetesTracker/Views/Glucose/GlucoseTrendView.swift
@@ -0,0 +1,25 @@
+//
+//  GlucoseTrendView.swift
+//  DiabetesTracker
+//
+//  Created by James Greiner on 1/17/24.
+//
+
+import SwiftUI
+
+struct GlucoseTrendView: View {
+    var body: some View {
+        Rectangle()
+            .foregroundStyle(.cyan)
+            .frame(width: 200, height: 200)
+        Spacer()
+    }
+}
+
+extension GlucoseTrendView {
+    
+}
+
+#Preview {
+    GlucoseTrendView()
+}
